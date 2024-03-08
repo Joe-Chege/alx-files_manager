3e99da0fe0036081a93f43723ac20fe22ef7c57b
Joe-Chege <njorogejosephchege@gmail.com>
2024-03-08 13:28:04 -0800

    updating the DB and redis

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-// server.js
-
 import express from 'express';
-import dotenv from 'dotenv';
-import routes from './routes';
+import router from './routes/index';
 
-dotenv.config();
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
+
+export default app;
